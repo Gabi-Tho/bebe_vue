0d52d3736cc010908ffd85a64e258b30d372b069
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-03-14 19:54:37 -0400

    add image et icones de la menu secondaire

--- bebe_vue/src/views/BouteilleCellierView.vue
+++ bebe_vue/src/views/BouteilleCellierView.vue
@@ -1,4 +1,46 @@
+<script setup>
+import IconContainer from '../components/IconContainer.vue'
+import IconRecherchePetit from '../components/icons/IconRecherchePetit.vue' 
+import IconCellier from '../components/icons/IconCellier.vue'
+import IconInfoPersonnelle from '../components/icons/IconInfoPersonnelle.vue'
+import IconCreerCellier from '../components/icons/IconCreerCellier.vue'
+</script>
+
 <template>
+                     
+    <nav>
+      <div class="menu-cellier-container">
+        <a href="{{ route('cellier.create') }}" class="flower-button">
+          <IconContainer>
+            <template #icon>
+               <IconCreerCellier />
+            </template>
+          </IconContainer>
+        </a>
+        <a href="" class="cellier-button">
+          <IconContainer>
+            <template #icon>
+               <IconCellier />
+            </template>
+          </IconContainer>
+        </a>
+        <a href="" class="search-button">
+          <IconContainer>
+            <template #icon>
+               <IconRecherchePetit />
+            </template>
+          </IconContainer>
+        </a>
+
+        <a href="" class="profile-button">
+          <IconContainer>
+            <template #icon>
+               <IconInfoPersonnelle />
+            </template>
+          </IconContainer>
+        </a>
+      </div>
+    </nav> 
     <div class="liste-container">
   
   <!-- search bar -->
@@ -12,8 +54,8 @@
 
     <div class="card">
         <div class="card-body">
-            <img src="" alt="nom">
-            <picture class="modal"><img src="" alt="img"></picture>
+            <img src="../assets/images/vin2.png" alt="nom">
+            <picture class="modal"><img src="../assets/images/vin2.png" alt="img"></picture>
             <div class="card-info">
                 <div class="card-info-title">
                     <h3 class="card-title">Nom</h3>
@@ -33,85 +75,56 @@
         </div>
 
     </div>
-
-  <div class="card">
+    <div class="card">
         <div class="card-body">
-
-            <img src="{{ asset('img/vin2.png')}}" alt="Product Image">
-            <picture class="modal"><img src="{{ asset('img/vin2.png')}}" alt="img"></picture>
+            <img src="../assets/images/vin2.png" alt="nom">
+            <picture class="modal"><img src="../assets/images/vin2.png" alt="img"></picture>
             <div class="card-info">
                 <div class="card-info-title">
-  
                     <h3 class="card-title">Riesling Pinot gris Mosel</h3>
                     <p class="card-subtitle">White wine 750 ml</p>
+                    <p class="card-subtitle">France</p>
                 </div>
                 <div class="card-info-client">
-                    
-                    <div>
-                    <p class="card-count">Quantite: 10 <button  class="card-btn_add" value="">&#8679;</button></p>
-                    </div>
-                    
+                    <p class="card-count">28 $</p>
                     <p class="card-rating">Note: &#9733;&#9733;&#9733;&#10025;</p>
+                    <div class="card-footer">
+                      <button  class="card-btn_add " value="">+</button>   
+                      <button  class="card-btn_add deleteModalBtn" value="" data-id="{{ $bouteille->id}}" data-dialog-id="deleteModal_{{ $bouteille->id }}">-</button>   
                 </div>
-          
-            </div>
+                </div>  
+            </div> 
+  
         </div>
-        <div class="card-footer">
 
-            <button  class="card-btn" value="">Drink</button>
-            
-        </div>
     </div>
     <div class="card">
         <div class="card-body">
-
-            <img src="{{ asset('img/vin2.png')}}" alt="Product Image">
-            <picture class="modal"><img src="{{ asset('img/vin2.png')}}" alt="img"></picture>
+            <div class="liste-image"></div>
+            <img class="img" src="../assets/images/vin2.png" alt="nom">
+            <picture class="modal"><img src="../assets/images/vin2.png" alt="img"></picture>
             <div class="card-info">
                 <div class="card-info-title">
-  
                     <h3 class="card-title">Riesling Pinot gris Mosel</h3>
                     <p class="card-subtitle">White wine 750 ml</p>
+                    <p class="card-subtitle">France</p>
                 </div>
                 <div class="card-info-client">
-  
-                    <p class="card-count">Quantite: 10 </p>
+                    <p class="card-count">28 $</p>
                     <p class="card-rating">Note: &#9733;&#9733;&#9733;&#10025;</p>
+                    <div class="card-footer">
+                      <button  class="card-btn_add " value="">+</button>   
+                      <button  class="card-btn_add deleteModalBtn" value="" data-id="{{ $bouteille->id}}" data-dialog-id="deleteModal_{{ $bouteille->id }}">-</button>   
                 </div>
-          
-            </div>
-        </div>
-        <div class="card-footer">
-
-            <button  class="card-btn" value="">Drink</button>
-            <button  class="card-btn" value="">Add</button>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-body">
-
-            <img src="{{ asset('img/vin2.png')}}" alt="Product Image">
-            <picture class="modal"><img src="{{ asset('img/vin2.png')}}" alt="img"></picture>
-            <div class="card-info">
-                <div class="card-info-title">
+                </div>  
+            </div> 
   
-                    <h3 class="card-title">Riesling Pinot gris Mosel</h3>
-                    <p class="card-subtitle">White wine 750 ml</p>
-                </div>
-                <div class="card-info-client">
-  
-                    <p class="card-count">Quantite: 10 </p>
-                    <p class="card-rating">Note: &#9733;&#9733;&#9733;&#10025;</p>
-                </div>
-          
-            </div>
         </div>
-        <div class="card-footer">
 
-            <button  class="card-btn" value="">Drink</button>
-            <button  class="card-btn" value="">Add</button>
-        </div>
     </div>
+
+  
+    
     
   </div>
 
